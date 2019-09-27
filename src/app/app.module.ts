13ecfc9dd6157a1166a37b96952b12a498c71e5b
AndresSuarez <andres19171@hotmail.es>
2019-09-27 18:37:02 -0500

    Cuartocommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app.routing';
 import { ThemeComponent } from './theme/theme.component';
 import { AdministrationComponent } from './pages/administration/administration.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
@@ -14,8 +14,7 @@ import { SettingsComponent } from './pages/settings/settings.component';
     AppComponent,
     LoginComponent,
     ThemeComponent,
-    AdministrationComponent,
-    DashboardComponent,
+    AdministrationComponent,   
     SettingsComponent
   ],
   imports: [
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,30 @@
 import { NgModule, Component } from "@angular/core";
 import {Routes,RouterModule}from "@angular/router";
 import { LoginComponent } from './login/login.component';
+import { ThemeComponent } from './theme/theme.component';
 export const routes:Routes=[{
     path:'login',
     component:LoginComponent
-}]
+},
+{
+path:'pages',component:ThemeComponent,
+children:[
+    {
+        path:'',
+        loadChildren:'./pages/administration/administration.module#AdministrationModule'
+    }
+    ,
+    {
+        path:'',
+        loadChildren:'./pages/dashboard/dashboard.module#DashboardModule'
+    },
+    {
+        path:'',
+        loadChildren:'./pages/settings/settings.module#SettingsModule'
+    }
+]
+}
+]
 @NgModule({
     imports:[RouterModule.forRoot(routes,{useHash:true})],
     exports:[RouterModule],
