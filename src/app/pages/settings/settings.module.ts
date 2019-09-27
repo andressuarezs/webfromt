5dec815565cecba7ff3cfba2e38386669685b610
AndresSuarez <andres19171@hotmail.es>
2019-09-27 17:05:42 -0500

    Tercercommit

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
+import { AreasComponent } from './areas/areas.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SectionsComponent } from './sections/sections.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AreasComponent, AuthorsComponent, BooksComponent, CategoriesComponent, SectionsComponent],
   imports: [
     CommonModule,
     SettingsRoutingModule
